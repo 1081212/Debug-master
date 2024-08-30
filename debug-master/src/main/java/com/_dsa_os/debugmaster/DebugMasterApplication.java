fa66490a0f00c215373402741a9c62e9c4b12ed3
jingchiyuan <jingchiyuan@4paradigm.com>
2024-08-30 18:07:48 +0800

    spring init

---
+++ debug-master/src/main/java/com/_dsa_os/debugmaster/DebugMasterApplication.java
@@ -0,0 +1,13 @@
+package com._dsa_os.debugmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebugMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebugMasterApplication.class, args);
+	}
+
+}
---
+++ debug-master/src/test/java/com/_dsa_os/debugmaster/DebugMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com._dsa_os.debugmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebugMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
